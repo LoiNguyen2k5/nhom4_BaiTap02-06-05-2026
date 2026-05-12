cc827856b3cf754d6481b1f15201e4a710119024
LoiNguyen <tiemnet.coaching.y3@gmail.com>
2026-05-12 13:38:44 +0700

    Hoàn thành UI cho ForgotPassword ( LoiNguyen)

--- src/config/initDb.js
+++ src/config/initDb.js
@@ -14,9 +14,7 @@ const initializeDatabase = async () => {
         port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
         logging: false,
-        dialectOptions: {
-          socketPath: '/tmp/mysql.sock',
-        },
+        dialectOptions: {},
       }
     );
 
